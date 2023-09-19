fbb0bcb410da43e1b3c065a2466f00ab7e533c07
tnxpp <mytnxpp@email.com>
2023-09-19 23:37:20 +0700

    beerkak

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -5,6 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
+
 export class DashboardComponent {
+imgprivacy1 = "./assets/images/condo-pic-1.png";
+imgprivacy2 = "./assets/images/condo-pic-2.png";
+imgprivacy3 = "./assets/images/condo-pic-3.jpg";
 
 }
+
