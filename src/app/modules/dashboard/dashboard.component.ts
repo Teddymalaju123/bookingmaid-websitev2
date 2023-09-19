4005b157a044f3e9a8aac74abc7cae17eae62d84
TendoCryzx <amfinezxc@hotmail.com>
2023-09-20 00:17:31 +0700

    add

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 imgprivacy1 = "./assets/images/condo-pic-1.png";
 imgprivacy2 = "./assets/images/condo-pic-2.png";
 imgprivacy3 = "./assets/images/condo-pic-3.jpg";
 
+ngOnInit(): void {
+  this.startCarouselAutoSlide();
+
+     }
+    startCarouselAutoSlide() {
+      setInterval(() => {
+        $('#myCarousel').carousel('next');
+      }, 3000); // Change image every 3 seconds
+    }
+    
 }
+
 
